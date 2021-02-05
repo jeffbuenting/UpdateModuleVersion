9ba274e31b61873300fb869a7dd47c1d2adb1014
jeff buenting <jeffbuenting@gmail.com>
2021-02-05 17:46:04 -0500

    comment most

--- PSScript.ps1
+++ PSScript.ps1
@@ -3,36 +3,36 @@ $File = get-childitem . | where Name -EQ "$(($env:GITHUB_REPOSITORY -split '/')[
 
 Write-output "PSD File = $($File.FullName)"
 
-Try {
-    $PSD = Get-Content $File.FullName -ErrorAction Stop
-}
-Catch {
-    Write-Error "Error getting manifest"
-    Exit 1
-}
-
-Try {
-    $CurrentVersion = $PSD | Select-String -Pattern "ModuleVersion = '(.*)'" -ErrorAction Stop | foreach { $_.Matches.Groups[1].Value }
-}
-Catch {
-    Write-Error "Error finding ModuleVersion."
-    Exit 1
-}
-          
-Write-Output "CurrentVersion = $CurrentVertion"
-
-
-$SplitVer = $CurrentVersion -split '.'
-$NewVer = "$SplitVer[0].$SplitVer[1].$([int]$SplitVer[2] + 1)"
-
-Write-Output "Updating to $NewVer"         
-
-Try {    
-    Update-ModuleManifest -Path "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1" -ModuleVersion $NewVer -ErrorAction Stop
-}
-Catch {
-    Write-Error "Error updating manifest file."
-    Exit 1
-}
+#Try {
+#    $PSD = Get-Content $File.FullName -ErrorAction Stop
+#}
+#Catch {
+#    Write-Error "Error getting manifest"
+#    Exit 1
+#}
+#
+#Try {
+#    $CurrentVersion = $PSD | Select-String -Pattern "ModuleVersion = '(.*)'" -ErrorAction Stop | foreach { $_.Matches.Groups[1].Value }
+#}
+#Catch {
+#    Write-Error "Error finding ModuleVersion."
+#    Exit 1
+#}
+#          
+#Write-Output "CurrentVersion = $CurrentVertion"
+#
+#
+#$SplitVer = $CurrentVersion -split '.'
+#$NewVer = "$SplitVer[0].$SplitVer[1].$([int]$SplitVer[2] + 1)"
+#
+#Write-Output "Updating to $NewVer"         
+#
+#Try {    
+#    Update-ModuleManifest -Path "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1" -ModuleVersion $NewVer -ErrorAction Stop
+#}
+#Catch {
+#    Write-Error "Error updating manifest file."
+#    Exit 1
+#}
 
 Exit 0
