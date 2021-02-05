d1dcd4f29f41c82189af7af7b62d97acb80aac90
jeff buenting <jeffbuenting@gmail.com>
2021-02-05 11:51:20 -0500

    created dockerfile and actions.ps1

---
+++ Action.ps1
@@ -0,0 +1,13 @@
+﻿$env:GITHUB_WORKSPACE
+
+Get-ChildItem -Path $env:GITHUB_WORKSPACE
+
+#$PSD = Get-Content "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1";
+#$CurrentVersion = $PSD | Select-String -Pattern "ModuleVersion = '(.*)'" | foreach { $_.Matches.Groups[1].Value }
+#          
+#Write-Output "CurrentVersion = $CurrentVertion";
+#$SplitVer = $CurrentVersion -split '.';
+#$NewVer = "$SplitVer[0].$SplitVer[1].$([int]SplitVer[2] + 1)"
+#          
+#
+#Update-ModuleManifest -Path "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1" -ModuleVersion $NewVer
