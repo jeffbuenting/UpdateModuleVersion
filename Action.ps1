bc712736b037137c1661f9caf664d84e238f3d77
jeff buenting <jeffbuenting@gmail.com>
2021-02-05 12:39:19 -0500

    version update

--- Action.ps1
+++ Action.ps1
@@ -1,13 +1,11 @@
-﻿$env:GITHUB_WORKSPACE
+﻿Write-Output "Updating Version on $($env:GITHUB_REPOSITORY -split '/')[-1]).psd1"
 
-Get-ChildItem -Path $env:GITHUB_WORKSPACE
+$PSD = Get-Content "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1"
+$CurrentVersion = $PSD | Select-String -Pattern "ModuleVersion = '(.*)'" | foreach { $_.Matches.Groups[1].Value }
+          
+Write-Output "CurrentVersion = $CurrentVertion"
+$SplitVer = $CurrentVersion -split '.'
+$NewVer = "$SplitVer[0].$SplitVer[1].$([int]$SplitVer[2] + 1)"
 
-#$PSD = Get-Content "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1";
-#$CurrentVersion = $PSD | Select-String -Pattern "ModuleVersion = '(.*)'" | foreach { $_.Matches.Groups[1].Value }
-#          
-#Write-Output "CurrentVersion = $CurrentVertion";
-#$SplitVer = $CurrentVersion -split '.';
-#$NewVer = "$SplitVer[0].$SplitVer[1].$([int]SplitVer[2] + 1)"
-#          
-#
-#Update-ModuleManifest -Path "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1" -ModuleVersion $NewVer
+Write-Output "Updating to $NewVer"          
+Update-ModuleManifest -Path "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1" -ModuleVersion $NewVer
