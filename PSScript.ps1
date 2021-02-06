555082fb86f3f457069189e8da1357772ff24aa2
jeff buenting <jeffbuenting@gmail.com>
2021-02-05 20:08:21 -0500

    uncommented all

--- PSScript.ps1
+++ PSScript.ps1
@@ -5,36 +5,34 @@ Write-output "PSD File = $($File.FullName)"
 
 Try {
     $PSD = Get-Content $File.FullName -ErrorAction Stop
-
-    $PSD
 }
 Catch {
     Write-Error "Error getting manifest"
     Exit 1
 }
 
-#Try {
-#    $CurrentVersion = $PSD | Select-String -Pattern "ModuleVersion = '(.*)'" -ErrorAction Stop | foreach { $_.Matches.Groups[1].Value }
-#}
-#Catch {
-#    Write-Error "Error finding ModuleVersion."
-#    Exit 1
-#}
-#          
-#Write-Output "CurrentVersion = $CurrentVertion"
-#
-#
-#$SplitVer = $CurrentVersion -split '.'
-#$NewVer = "$SplitVer[0].$SplitVer[1].$([int]$SplitVer[2] + 1)"
-#
-#Write-Output "Updating to $NewVer"         
-#
-#Try {    
-#    Update-ModuleManifest -Path "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1" -ModuleVersion $NewVer -ErrorAction Stop
-#}
-#Catch {
-#    Write-Error "Error updating manifest file."
-#    Exit 1
-#}
+Try {
+    $CurrentVersion = $PSD | Select-String -Pattern "ModuleVersion = '(.*)'" -ErrorAction Stop | foreach { $_.Matches.Groups[1].Value }
+}
+Catch {
+    Write-Error "Error finding ModuleVersion."
+    Exit 1
+}
+          
+Write-Output "CurrentVersion = $CurrentVertion"
+
+
+$SplitVer = $CurrentVersion -split '.'
+$NewVer = "$SplitVer[0].$SplitVer[1].$([int]$SplitVer[2] + 1)"
+
+Write-Output "Updating to $NewVer"         
+
+Try {    
+    Update-ModuleManifest -Path "$($env:GITHUB_REPOSITORY -split '/')[-1]).psd1" -ModuleVersion $NewVer -ErrorAction Stop
+}
+Catch {
+    Write-Error "Error updating manifest file."
+    Exit 1
+}
 
 Exit 0
